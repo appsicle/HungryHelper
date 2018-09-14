bd2306850838ebf9601cf0c811d20f74c367e064
System Administrator <root@Alberts-MacBook-Air.local>
2018-09-14 01:59:24 -0700

    refactor

---
+++ src/components/Input.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <form action="">
+            <input type="text">
+        </form>
+        <p>hello</p>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
