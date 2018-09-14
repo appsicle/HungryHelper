27c36113dc4f89c2cb67409d156010c105db76f1
justinekoa <justinekoa@gmail.com>
2018-09-14 01:53:25 -0700

    basic working get recipe button

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,13 @@
             <v-toolbar-title>hello</v-toolbar-title>
             <v-btn flat>View</v-btn>
             <v-btn flat @click="runCode">hello</v-btn>
-            {{text}}
+
+            <v-btn flat @click="getRecipe">get recipe</v-btn>
         </v-toolbar>
         <v-container>
+            <p v-for="item in instructions">
+                {{item[0]}} <br> {{item[1]}}
+            </p>
 
         </v-container>
         <main>
@@ -21,7 +25,8 @@
 
         data() {
             return {
-                text: 'asd'
+                // text: [];
+                instructions: []
             }
         }
         ,
@@ -37,7 +42,44 @@
                 }).then(response => {
                     console.log(response.body);
                 })
+            },
+
+            getRecipe(){
+                this.$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=2&ranking=1",
+                    {
+                        headers: {
+                            "X-Mashape-Key": "1YEwePZsOkmshgRlK83kandA6m0Rp1Qv3qJjsn3MHTKj6twMXE",
+                            "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+                        }
+                    }).then(response => {
+                        var text = [];
+                        let dictionary = response.body;
+                        for (var key in dictionary){
+                            text.push(dictionary[key]["id"]);
+                        }
+
+                        for (var id in text){
+                            this.$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/"+text[id]+"/information?includeNutrition=false",
+                                {
+                                    headers: {
+                                        "X-Mashape-Key": "1YEwePZsOkmshgRlK83kandA6m0Rp1Qv3qJjsn3MHTKj6twMXE",
+                                        "Accept": "application/json"
+                                    }
+                                }).then(response => {
+                                    var info = response.body;
+                                    var list = [info["title"], info["instructions"]];
+                                    this.instructions.push(list);
+                            })
+                        }
+
+                })
+
+
+
+
             }
+
+
 
         }
     }
