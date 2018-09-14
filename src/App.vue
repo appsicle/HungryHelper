41874c2051cf3bd1406f271664731d9b4c743fb2
System Administrator <root@Alberts-MacBook-Air.local>
2018-09-14 02:11:14 -0700

    refactor v2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
     <v-app light>
-        <v-toolbar>
-            <v-toolbar-title>hello</v-toolbar-title>
-            <v-btn flat>View</v-btn>
-            <v-btn flat @click="runCode">hello</v-btn>
-
-            <v-btn flat @click="getRecipe">get recipe</v-btn>
-        </v-toolbar>
+        <Header @passed="setValue"></Header>
         <v-container>
             <p v-for="item in instructions">
                 {{item[0]}} <br> {{item[1]}}
@@ -20,66 +14,21 @@
 </template>
 
 <script>
-
+    import Header from './components/Header.vue'
     export default {
-
+        components: {
+            Header
+        },
         data() {
             return {
                 instructions: []
             }
-        }
-        ,
+        },
         methods: {
-
-
-            runCode() {
-                this.$http.get('https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/trivia/random', {
-                    headers: {
-                        "X-Mashape-Key": "1YEwePZsOkmshgRlK83kandA6m0Rp1Qv3qJjsn3MHTKj6twMXE",
-                        "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-                    }
-                }).then(response => {
-                    console.log(response.body);
-                })
-            },
-
-            getRecipe(){
-                this.$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=2&ranking=1",
-                    {
-                        headers: {
-                            "X-Mashape-Key": "1YEwePZsOkmshgRlK83kandA6m0Rp1Qv3qJjsn3MHTKj6twMXE",
-                            "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
-                        }
-                    }).then(response => {
-                        var text = [];
-                        let dictionary = response.body;
-                        for (var key in dictionary){
-                            text.push(dictionary[key]["id"]);
-                        }
-
-                        for (var id in text){
-                            this.$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/"+text[id]+"/information?includeNutrition=false",
-                                {
-                                    headers: {
-                                        "X-Mashape-Key": "1YEwePZsOkmshgRlK83kandA6m0Rp1Qv3qJjsn3MHTKj6twMXE",
-                                        "Accept": "application/json"
-                                    }
-                                }).then(response => {
-                                    var info = response.body;
-                                    var list = [info["title"], info["instructions"]];
-                                    this.instructions.push(list);
-                            })
-                        }
-
-                })
-
-
-
-
+            setValue(value){
+                this.instructions.push(value);
             }
-
-
-
         }
+
     }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,51 @@
 <template>
     <v-toolbar>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title>Title</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-toolbar-items class="hidden-sm-and-down">
-            <v-btn flat>Link One</v-btn>
-            <v-btn flat>Link Two</v-btn>
-            <v-btn flat>Link Three</v-btn>
-        </v-toolbar-items>
+        <v-toolbar-title>hello</v-toolbar-title>
+        <v-btn flat>View</v-btn>
+        <v-btn flat @click="getRecipe">get recipe</v-btn>
     </v-toolbar>
 </template>
 
 <script>
-    export default {}
+    export default {
+        data(){
+            return{
+                // instructions: []
+            }
+        },
+        methods: {
+            getRecipe(){
+                this.$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=apples%2Cflour%2Csugar&limitLicense=false&number=2&ranking=1",
+                    {
+                        headers: {
+                            "X-Mashape-Key": "1YEwePZsOkmshgRlK83kandA6m0Rp1Qv3qJjsn3MHTKj6twMXE",
+                            "X-Mashape-Host": "spoonacular-recipe-food-nutrition-v1.p.mashape.com"
+                        }
+                    }).then(response => {
+                    var text = [];
+                    let dictionary = response.body;
+                    for (var key in dictionary){
+                        text.push(dictionary[key]["id"]);
+                    }
+
+                    for (var id in text){
+                        this.$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/"+text[id]+"/information?includeNutrition=false",
+                            {
+                                headers: {
+                                    "X-Mashape-Key": "1YEwePZsOkmshgRlK83kandA6m0Rp1Qv3qJjsn3MHTKj6twMXE",
+                                    "Accept": "application/json"
+                                }
+                            }).then(response => {
+                            var info = response.body;
+                            var list = [info["title"], info["instructions"]];
+                            // this.instructions.push(list);
+                            this.$emit('passed', list)
+                        })
+                    }
+
+                })
+
+            }
+        }
+    }
 </script>
